ceaceb779e6837c3a97601ebd945d87b3062c4d9
Nikil245 <nikilsuvarna23@gmail.com>
2026-09-14 22:17:55 +0530

    Started with the dsa in leetcode question number 3

---
+++ DSA/Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        Set<Character> charSet = new HashSet<>();
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (!charSet.contains(s.charAt(right))) {
+                charSet.add(s.charAt(right));
+                maxLength = Math.max(maxLength, right - left + 1);
+            } else {
+                while (charSet.contains(s.charAt(right))) {
+                    charSet.remove(s.charAt(left));
+                    left++;
+                }
+                charSet.add(s.charAt(right));
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String test = "abcabcbb";
+        int result = sol.lengthOfLongestSubstring(test);
+        System.out.println("Input: \"" + test + "\" -> Longest substring length: " + result);
+    }
+}
